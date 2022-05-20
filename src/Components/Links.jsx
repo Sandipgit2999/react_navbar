0dcd99e2f60d55f327cb826ac23d281c38eaeed4
sandip suryawanshi <suryawanshisandip299@gmail.com>
2022-05-20 19:40:32 +0530

    react_navbar

---
+++ src/Components/Links.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css';
+
+const Links = () => {
+  return (
+    <div className="link">
+      <p>Services</p>
+      <p>Products</p>
+      <p>About</p>
+    </div>
+  );
+};
+
+export default Links;
